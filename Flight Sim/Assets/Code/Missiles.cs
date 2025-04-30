0597bc9e0a0f80fd0d546b0e3d836fc5b6c5cd12
Mapp7777 <77149474+Mapp7777@users.noreply.github.com>
2025-04-30 20:54:55 +0200

    Update Missiles.cs

--- Flight Sim/Assets/Code/Missiles.cs
+++ Flight Sim/Assets/Code/Missiles.cs
@@ -8,7 +8,7 @@ public class Missiles : MonoBehaviour
 
     private Rigidbody rb;
 
-    public float rotationSpeed; 
+  
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -42,8 +42,5 @@ public class Missiles : MonoBehaviour
     }
 
 
-    void OnCollisionEnter(Collision col)
-    {
-        Destroy(this.gameObject);
-    }
+  
 }
